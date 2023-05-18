432ff76d69e353b328e7037592b765f8af4b32d6
Dina Mamdouh <63278084+DinaSol@users.noreply.github.com>
2023-05-19 02:40:43 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,38 +1,29 @@
+
 resource "aws_s3_bucket" "web-s3" {
   bucket = "web-s3.com"
-
-  tags = {
-    Name        = "web-s3"
+  acl    = "public-read"
+  website {
+    index_document = "index.html"
+    error_document = "index.html"
   }
 }
 
-resource "aws_s3_bucket_website_configuration" "example" {
+resource "aws_s3_bucket_policy" "my_bucket_policy" {
   bucket = aws_s3_bucket.web-s3.id
 
-  index_document {
-    suffix = "index.html"
-  }
-
-  error_document {
-    key = "index.html"
-  }
-  }
-
-
-resource "aws_s3_bucket_policy" "example_bucket_policy" {
-  bucket = "web-s3.com"
-
   policy = jsonencode({
-    "Version": "2012-10-17",
-    "Id": "Policy1679932274230",
-    "Statement": [
-        {
-            "Sid": "Stmt1679932268557",
-            "Effect": "Allow",
-            "Principal": "*",
-            "Action": "s3:GetObject",
-            "Resource": "aws_s3_bucket.web-s3.id/*"
-        }
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = "*"
+        Action = [
+          "s3:GetObject"
+        ]
+        Resource = [
+          "${aws_s3_bucket.web-s3.arn}/*"
+        ]
+      }
     ]
-})
+  })
 }

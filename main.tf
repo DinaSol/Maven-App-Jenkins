9897b1ae185b5899458740a9230d2f3327dfe6d9
Dina Mamdouh <63278084+DinaSol@users.noreply.github.com>
2023-05-19 02:52:25 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,24 @@
 
 resource "aws_s3_bucket" "web-s3" {
   bucket = "web-s3.com"
-  acl    = "public-read"
-  website {
-    index_document = "index.html"
-    error_document = "index.html"
+}
+resource "aws_s3_bucket_website_configuration" "web-s3-website" {
+  bucket = aws_s3_bucket.web-s3.id
+
+  index_document {
+    suffix = "index.html"
   }
+  error_document {
+    key = "index.html"
+  }
+}
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.web-s3.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
 
 resource "aws_s3_bucket_policy" "my_bucket_policy" {

fdd65ba1d1f8f8291864daa34a229369bf6685ab
Dina Mamdouh <63278084+DinaSol@users.noreply.github.com>
2023-05-19 02:28:17 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ resource "aws_s3_bucket_policy" "example_bucket_policy" {
             "Effect": "Allow",
             "Principal": "*",
             "Action": "s3:GetObject",
-            "Resource": "aws_s3_bucket.web-s3.arn/*"
+            "Resource": "aws_s3_bucket.web-s3.id/*"
         }
     ]
 })
